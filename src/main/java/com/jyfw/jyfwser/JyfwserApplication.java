8d1a17af0390944e36b2e1e03200de8c581930fb
e <17816853571@163.com>
2018-01-15 16:28:40 +0800

    初始化项目;

---
+++ src/main/java/com/jyfw/jyfwser/JyfwserApplication.java
@@ -0,0 +1,12 @@
+package com.jyfw.jyfwser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyfwserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyfwserApplication.class, args);
+	}
+}
