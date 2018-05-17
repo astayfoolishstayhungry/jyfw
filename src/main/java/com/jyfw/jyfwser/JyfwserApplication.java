f6a5c7c9e4c9aed58ec3206d233106c6786949a8
e <17816853571@163.com>
2018-05-17 20:42:28 +0800

    tomcat

--- src/main/java/com/jyfw/jyfwser/JyfwserApplication.java
+++ src/main/java/com/jyfw/jyfwser/JyfwserApplication.java
@@ -2,9 +2,16 @@ package com.jyfw.jyfwser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JyfwserApplication {
+public class JyfwserApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JyfwserApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JyfwserApplication.class, args);
