62f365df6f5fa4ed286224aa65d34e88ecafcf3f
e <17816853571@163.com>
2018-05-24 21:08:12 +0800

    up;

---
+++ src/main/java/com/jyfw/jyfwser/service/TopicService.java
@@ -0,0 +1,13 @@
+package com.jyfw.jyfwser.service;
+
+import com.jyfw.jyfwser.pojo.entity.TopicEntity;
+
+import java.util.List;
+
+public interface TopicService {
+
+    Integer insertTopic(TopicEntity topic);
+
+    List<TopicEntity> listTopic(Integer page, Integer count, String topicName);
+
+}
